da8abf1473d01ca9bcc84ce8f449ab7046b86ff8
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-12-03 22:44:02 +0300

    commit tree 1b023057faaf5925bbf11a924cd054ed3656c7f8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+
+
+function saturdayFun(activity = "roller-skate"){
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+
+function mondayWork(work = "go to the office"){
+  return (`This Monday, I will ${work}.`);
+}
+mondayWork();
+
+/*
+function wrapAdjective(result = '*'){
+let emphatic = function (){
+    return `You are ${result}a hard worker${result}!`; 
+}
+return `You are ${result}a dedicated programmer${result}!`;
+return emphatic;
+}
+*/
+
+let wrapAdjective = function(result='*'){
+  return function(adj="new"){
+    return `You are ${result}${adj}${result}!`; 
+  }
+}
