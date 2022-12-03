db965dfa5fc711c142aa95e301aa7f577f05be24
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-12-03 22:47:05 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+
+
+function saturdayFun(activity = "roller-skate"){
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+
+function mondayWork(work = "go to the office"){
+  return (`This Monday, I will ${work}.`);
+}
+mondayWork();
+
+
+let wrapAdjective = function(result='*'){
+  return function(adj="new"){
+    return `You are ${result}${adj}${result}!`; 
+  }
+}
+
+/*
+function wrapAdjective(result = '*'){
+let emphatic = function (){
+    return `You are ${result}a hard worker${result}!`; 
+}
+return `You are ${result}a dedicated programmer${result}!`;
+return emphatic;
+}
+*/
